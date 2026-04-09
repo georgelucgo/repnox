d06fba6c23339ae02d591939d7a2feccbc6b4852
George Lucas <152038729+georgelucgo@users.noreply.github.com>
2026-04-08 20:47:07 -0400

    iniciando

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function Home() {
+ 
+
+  return (
+    <div>
+      <h1>Repnox</h1>
+
+      <div>
+        <h2>Exercicios</h2>
+        <p>Braço</p>
+        <p>Peito</p>
+        <p>Perna</p>
+        <p>Triceps</p>
+        <p>Costas</p>
+        <p>Abdomen</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
